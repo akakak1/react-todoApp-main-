fb86fc66754bfe4869b575e143963e50717ae8ab
Amit <Amit@example.com>
2018-01-20 19:15:14 +0530

    Completed handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList =require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
 
@@ -11,22 +12,30 @@ var TodoApp = React.createClass({
             searchText:'',
             todos:[
                 {
-                    id:1,
-                    text:'Walk the dog.'
+                    id: uuid(),
+                    text: 'Walk the dog.'
                 },
                 {
-                    id:2,
-                    text:'Clean the yard.'
+                    id: uuid(),
+                    text: 'Clean the yard.'
                 },
                 {
-                    id:3,
-                    text:'Its still working.'
+                    id: uuid(),
+                    text: 'Its still working.'
                 }
             ]
         }
     },
     handleAddTodo:function (text){
-        alert('new todo: ' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        })
     },
     handleSearch: function(showCompleted, searchText){
         this.setState({
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,16 @@ describe('TodoApp', ()=>{
     it('should exist', ()=>{
         expect(TodoApp).toExist();
     })
+
+    describe('handleAddTodo', ()=>{
+        it('should add todo to the todos state when handleAddTodo', ()=>{
+            var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+            var todoText = 'test text';
+
+            todoApp.setState({todos:[]});
+            todoApp.handleAddTodo(todoText);
+
+            expect(todoApp.state.todos[0].text).toBe(todoText);
+        })
+    })
 })
