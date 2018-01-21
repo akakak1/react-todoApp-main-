d7aefaf0b80ced887e50438805c7c63fefd6527e
Amit <Amit@example.com>
2018-01-21 20:24:11 +0530

    Added test suits for onToggle and handleToggle

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -10,4 +10,22 @@ describe('Todo', ()=>{
     it('should exist', ()=>{
         expect(Todo).toExist();
     })
+
+    describe('onToggle', ()=> {
+        it('should call onToggle with an id when clicked', ()=> {
+            var todoData = {
+                id:199,
+                text:'Test features',
+                completed: false
+            };
+            var spy = expect.createSpy();
+            var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+            var $el = $(ReactDOM.findDOMNode(todo));
+
+            TestUtils.Simulate.click($el[0]);
+            
+            expect(spy).toHaveBeenCalledWith(199);
+
+        })
+    })
 })
