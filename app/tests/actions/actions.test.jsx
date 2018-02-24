be521d304787da6784ee9352adcfcf1d402bfdd2
Amit <Amit@example.com>
2018-02-24 11:49:23 +0530

    Add actions & actions test

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,48 @@
+var expect = require('expect');
+var actions = require('actions');
+
+
+describe('Action', () => {
+    it('should generate search text action', () => {
+        var action = {
+            type: 'SET_SEARCH_TEXT',
+            searchText: 'Some search text'
+        };
+
+        var res = actions.setSearchText(action.searchText);
+        
+        expect(res).toEqual(action);
+    });
+
+    it('should generate toggle show completed action', () => {
+        var action = {
+            type: 'TOGGLE_SHOW_COMPLETED',    // here we dont have comma in the action generator.... so what would happen if we use toBe() instead of toEqual() ;
+        }
+
+        var res = actions.toggleShowCompleted();
+
+        expect(res).toEqual(action);
+    })
+
+    it('should generate add todo action', () => {
+        var action = {
+            type: 'ADD_TODO',
+            text: 'Some todo'
+        }
+
+        var res = actions.addTodo(action.text);
+
+        expect(res).toEqual(action);
+    })
+
+    it('should genereate toggle todo action', () => {
+        var action = {
+            type: 'TOGGLE_TODO',
+            id: '123'
+        }
+
+        var res = actions.toggleTodo(action.id);
+
+        expect(res).toEqual(action);
+    })
+})
