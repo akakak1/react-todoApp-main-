a7314f24be0005a826eef52155d94fe1903dc741
Amit <Amit@example.com>
2018-02-24 15:32:51 +0530

    Added todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -31,4 +31,39 @@ describe('Reducers', () => {
             expect(res).toEqual(true);
         });
     });
+
+    describe('todoReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Walk the cat'
+            };
+
+            var res = reducers.todoReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle todo', () => {
+            var todos = [{
+                id: '123',
+                text: 'something',
+                completed: true,
+                createdAt: 123,
+                completedAt: 125
+            }]
+
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: '123'
+            }
+
+            var res = reducers.todoReducer(df(todos), df(action)) ;
+
+            expect(res[0].completed).toEqual(false);
+            expect(res[0].completedAt).toEqual(undefined);
+        })
+
+    })
 });
